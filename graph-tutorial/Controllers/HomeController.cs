0922db47128369ce84118fc9cee43981b353a135
WilliamK97 <william.kamarainen@gmail.com>
2019-11-13 11:18:09 +0100

    aa

--- graph-tutorial/Controllers/HomeController.cs
+++ graph-tutorial/Controllers/HomeController.cs
@@ -42,15 +42,26 @@ namespace graph_tutorial.Controllers
             var axel = await GraphHelper.GetBookings();
             var a = await GraphHelper.GetMe();
             var photo = await GraphHelper.GetMyPhoto();
+            
             ViewBag.Bookings = axel;
+            
             ViewBag.Mail = a.Mail;
             ViewBag.Name = a.DisplayName;
             ViewBag.Photo = photo;
+            
+
+            var folk = await GraphHelper.ListUser();
+            ViewBag.Users = folk;        
 
             return View();
         }
 
         [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            var folk = await GraphHelper.ListUser();
+            var list = folk.Select(_ => new SelectListItem() { Text = _.DisplayName, Value = _.Id}).ToList();
+            ViewBag.Users = list;
         public async Task<ActionResult> Create(string id)
         {
             if (!string.IsNullOrEmpty(id))
@@ -84,5 +95,21 @@ namespace graph_tutorial.Controllers
             return RedirectToAction("Test");
         }
 
+<<<<<<< HEAD
+=======
+
+        //public async Task<ActionResult> GetUser()
+        //{
+        //    var folk = await GraphHelper.ListUser();
+        //    ViewBag.Users = folk;
+
+        //    return View("Test"); 
+        //}
+
+
+
+
+
+>>>>>>> a454bac1dd1b4d6b4708c815aef9e5a04ced05da
     }
 }
