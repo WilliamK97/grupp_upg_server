9a3870dbe15c99bac54b665b2698d627256734a0
Axel Ohrås <axel.ohras@gmail.com>
2019-11-13 12:29:08 +0100

    Calendar grid

--- graph-tutorial/Controllers/CalendarController.cs
+++ graph-tutorial/Controllers/CalendarController.cs
@@ -1,4 +1,5 @@
 ﻿using graph_tutorial.Helpers;
+using graph_tutorial.Models;
 using System;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -23,6 +24,27 @@ namespace graph_tutorial.Controllers
             }
 
             return View(events);
+        }
+
+        public ActionResult Grid()
+        {
+            var model = new CalendarGridViewModel();
+            var now = DateTime.Now;
+            double days = DateTime.DaysInMonth(now.Year, now.Month);
+            double cols = 6;
+            model.Rows = (int)Math.Ceiling(days / cols);
+            model.Days = (int)days;
+            model.Cols = (int)cols;
+            model.Month = now.Month;
+            model.Year = now.Year;
+            return View(model);
+        }
+
+        public ActionResult Details(string d)
+        {
+            var date = DateTime.Parse(d);
+            ViewBag.Test = d;
+            return View();
         }
     }
 }
---
+++ graph-tutorial/Models/CalendarGridViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace graph_tutorial.Models
+{
+    public class CalendarGridViewModel
+    {
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+        public int Days { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
