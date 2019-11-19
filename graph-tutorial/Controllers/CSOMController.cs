d31f02d65ce17d71edb919e748a04eacf98d69b9
Axel Ohrås <axel.ohras@gmail.com>
2019-11-19 12:34:14 +0100

    csom update

---
+++ graph-tutorial/Controllers/CSOMController.cs
@@ -0,0 +1,57 @@
+﻿using graph_tutorial.Attributes;
+using graph_tutorial.Helpers;
+using graph_tutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    [AzureAuthenticate("https://localhost:44397/CSOM")]
+    public class CSOMController : BaseController
+    {
+        // GET: CSOM
+        public ActionResult Index()
+        {
+            var lists = SharepointHelper.GetLists();
+            ViewBag.Lists = lists.Select(_ => _.Title);
+            return View();
+        }
+
+        public ActionResult ListDetails(string listName)
+        {
+            var list = SharepointHelper.GetList(listName);
+            ViewBag.ListName = list.Title;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateList()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateList(CreateListViewModel model)
+        {
+            SharepointHelper.CreateList(model.Title);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CreateListItem(string listName)
+        {
+            var model = new CreateListItemViewModel();
+            model.ListTitle = listName;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CreateListItem(CreateListItemViewModel model)
+        {
+            SharepointHelper.AddItemToList(model.ListTitle, model.Title);
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ graph-tutorial/Helpers/SharepointHelper.cs
@@ -0,0 +1,88 @@
+﻿using graph_tutorial.TokenStorage;
+using Microsoft.Identity.Client;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace graph_tutorial.Helpers
+{
+    public static class SharepointHelper
+    {
+        private static string _Username = ConfigurationManager.AppSettings["sp:Username"];
+        private static string _Password = ConfigurationManager.AppSettings["sp:Password"];
+
+        private static ClientContext GetClient()
+        {
+            ClientContext context = new ClientContext("https://ohras.sharepoint.com");
+            context.Credentials = GetCredentials();
+            return context;
+        }
+
+        private static SharePointOnlineCredentials GetCredentials()
+        {
+            var securePassword = new SecureString();
+            foreach (var c in _Password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            return new SharePointOnlineCredentials(_Username, securePassword);
+        }
+
+        public static IEnumerable<List> GetLists()
+        {
+            using (var client = GetClient())
+            {
+                var query = client.Web.Lists;
+                client.Load(query);
+                client.ExecuteQuery();
+                return query;
+            }
+        }
+
+        public static List GetList(string title)
+        {
+            using (var client = GetClient())
+            {
+                var query = client.Web.Lists.GetByTitle(title);
+                client.Load(query);
+                client.ExecuteQuery();
+                return query;
+            }
+        }
+
+        public static void CreateList(string title)
+        {
+            using (var client = GetClient())
+            {
+                ListCreationInformation lci = new ListCreationInformation()
+                {
+                    Title = title,
+                    TemplateType = 100
+                };
+
+                var query = client.Web.Lists.Add(lci);
+                client.ExecuteQuery();
+            }
+        }
+
+        public static void AddItemToList(string listTitle, string title)
+        {
+            using (var client = GetClient())
+            {
+                var list = client.Web.Lists.GetByTitle(listTitle);
+                ListItemCreationInformation itemInfo = new ListItemCreationInformation();
+                var item = list.AddItem(itemInfo);
+                item["Title"] = title;
+                item.Update();
+                client.ExecuteQuery();
+            }
+        }
+    }
+}
